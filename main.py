b8102465884e06253dfe52d35e184efab0201c0a
Chris <reidcr@gmail.com>
2023-07-08 21:37:19 -0600

    env variable attempt #8

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ Bootstrap(app)
 # CONNECT TO DB
 # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
-SQLALCHEMY_DATABASE_URI = uri
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
